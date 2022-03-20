2e8e622eae50ff33ffe76534abc34ae25a6a13c2
syuan <syuan.jhong1989@gmail.com>
2022-03-20 19:09:52 +0800

    new

---
+++ climb.py
@@ -0,0 +1,43 @@
+
+from bs4 import BeautifulSoup
+from numpy import c_
+import requests,time
+
+#use request.get() to store web's html
+news1=requests.get('https://tw.stock.yahoo.com/quote/2317.TW/technical-analysis')
+news2=requests.get('https://tw.stock.yahoo.com/quote/2883.TW')
+#analysis the html code,and build the object
+soup1=BeautifulSoup(news1.text,'html.parser')
+soup2=BeautifulSoup(news2.text,'html.parser')
+#output the html content
+#print(soup.prettify())
+fbox=[]
+cbox=[]
+fox_buy=104.59
+cdi_buy=18.34
+def foxconn():
+    results1=soup1.find_all('div',class_='D(f) Ai(fe) Mb(4px)')#找全部或限制數量
+    for result in results1:
+        for i in result:
+            return i.text
+def cdibh():
+    results2=soup2.find_all('div',class_='D(f) Ai(fe) Mb(4px)')#找全部或限制數量
+    for result in results2:
+        for i in result:
+            return i.text
+
+def f_compare():
+    ans=fox_buy-float(foxconn())
+    
+    if ans<0:
+        return('🔴鴻海'+str(0-round(ans,3)))
+
+    elif ans>0:
+        return('🟢鴻海'+str(0-round(ans,3)))
+def c_compare():
+    ans1=cdi_buy-float(cdibh())
+    if ans1<0:
+        return('🔴開發金'+str(0-round(ans1,3)))
+
+    elif ans1>0:
+        return('🟢開發金'+str(0-round(ans1,3)))
---
+++ run.py
@@ -0,0 +1,43 @@
+import requests
+import climb
+import time
+import os
+token = '5zR1x3V9Gxt2ZOzBBa6icxo7dxSHRADSIRAcEDR8HLg'
+def lineNotifyMessage(token, msg):
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    payload = {'message': msg}
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload)
+    return r.status_code
+
+def lineNotify(token, msg, stickerPackageId, stickerId):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+   
+    payload = {"message": msg, "stickerPackageId": stickerPackageId, 'stickerId': stickerId}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
+ 
+if __name__=="__main__":
+    
+# 你要傳送的訊息內容
+    message = climb.c_compare()+'現價'+climb.cdibh()
+    message2 = climb.f_compare()+'現價'+climb.foxconn()
+# 將剛剛複製下來的Token取代以下''中的內容即可 
+    lineNotifyMessage(token, message)
+    lineNotifyMessage(token, message2)
+    '''
+    msg = "Hello Python"
+    stickerPackageId = 2
+    stickerId = 38
+ 
+    lineNotify(token, msg, stickerPackageId, stickerId)'''
+    time.sleep(60)
+
+
+
